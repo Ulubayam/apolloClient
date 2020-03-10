7baf61d8bbbb34685ac5bea982ee69af71b8df96
gizem.ulubayam <gizem.ulubayam@cimri.com>
2020-03-10 10:32:07 +0300

    fixed css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import "./App.css";
+
+const rickandmorty = gql`
+  {
+    characters {
+      results {
+        id
+        name
+        image
+        gender
+        species
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(rickandmorty);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p> Oppss !!</p>;
+  const newData = data.characters.results;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div>
+        {newData.map(item => {
+          return (
+            <div key={item.id} className="card">
+              <img
+                src={item.image}
+                alt="RickandMorty"
+                style={{ width: "100%" }}
+              />
+              <div className="container">
+                <h4>
+                  <b>{item.name}</b>
+                </h4>
+                <p>{item.species}</p>
+                <p>{item.gender}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
