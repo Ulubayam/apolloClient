b6418f623e90c228246d3da5d29800ed5469320c
gizem.ulubayam <gizem.ulubayam@cimri.com>
2020-07-21 14:04:34 +0300

    fixed css

--- src/App.js
+++ src/App.js
@@ -23,27 +23,24 @@ function App() {
   if (error) return <p> Oppss !!</p>;
   const newData = data.characters.results;
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div>
-        {newData.map(item => {
-          return (
-            <div key={item.id} className="card">
-              <img
-                src={item.image}
-                alt="RickandMorty"
-                style={{ width: "100%" }}
-              />
-              <div className="container">
-                <h4>
+    <div className="container">
+      <h1 className="title">Rick and Morty API with GraphQL</h1>
+      {newData.map((item) => {
+        return (
+          <div key={item.id} className="card">
+            <img src={item.image} alt="RickandMorty" />
+            <div className="container">
+              <div className="container-title">
+                <h3 style={{ width: "150px" }}>
                   <b>{item.name}</b>
-                </h4>
+                </h3>
                 <p>{item.species}</p>
                 <p>{item.gender}</p>
               </div>
             </div>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
